e52e430a5a32ecca5047d662de65feb70d263875
Shieru-ktc <ktc2231020001@edu.kyoto-tech.ac.jp>
2024-06-14 19:51:31 +0900

    6/14追加

---
+++ Assets/Editor/CreateSceneNames.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+/// <summary> 参考サイト https://www.mum-meblog.com/entry/research/scene_names_editor_extension </summary>
+
+public class CreateSceneNames : EditorWindow
+{
+    private static string ScriptsFolderPath => Application.dataPath + "/Scripts";
+
+    [MenuItem("Editor/Generate Scene Names Class")]
+    private static void GenerateSceneNamesClass()
+    {
+        Debug.Log("GenerateSceneNamesClass is executing...");
+        List<string> sceneNames = EditorBuildSettings.scenes
+            .Where(scene => scene.enabled)
+            .Select(scene => Path.GetFileNameWithoutExtension(scene.path))
+            .ToList();
+        StringBuilder log = new StringBuilder();
+        foreach (var scene in sceneNames)
+        {
+            log.Append($"{scene}\n");
+        }
+        Debug.Log($"Available scene names are\n{log}");
+
+        CreateSceneNamesClass(sceneNames);
+    }
+
+    private static void CreateSceneNamesClass(List<string> sceneNames)
+    {
+        if (Directory.Exists(ScriptsFolderPath + "/Config") == false)
+        {
+            Debug.Log("Scripts/Config directory not found. Create new...");
+            Directory.CreateDirectory(ScriptsFolderPath + "/Config");
+        }
+
+        if (File.Exists(ScriptsFolderPath + "/Config/SceneNames.cs") == false)
+        {
+            Debug.Log("Scripts/Config/SceneNames.cs not found. Create new...");
+            using (var sr = File.Create(ScriptsFolderPath + "/Config/SceneNames.cs"))
+            {
+            }
+        }
+        using (var sr = new StreamWriter(ScriptsFolderPath + "/Config/SceneNames.cs"))
+        {
+            sr.Write(GenerateClassContent(sceneNames));
+        }
+        AssetDatabase.Refresh();
+    }
+
+    private static string GenerateClassContent(List<string> sceneNames)
+    {
+        StringBuilder content = new();
+        content.Append("namespace Config {\n");
+        content.Append("    public class SceneNames {");
+        foreach (var sceneName in sceneNames)
+        {
+            content.Append($"\n        public static readonly string {sceneName} = \"{sceneName}\";");
+        }
+        content.Append("\n    }");
+        content.Append("\n}");
+        return content.ToString();
+    }
+}
---
+++ Assets/Scripts/Config/SceneNames.cs
@@ -0,0 +1,14 @@
+namespace Config {
+    public class SceneNames {
+        public static readonly string Title = "0_Title";
+        public static readonly string StageSelect = "1_StageSelect";
+        public static readonly string Default_Main = "Default_Main";
+        public static readonly string Main_Stage1 = "Main_Stage1";
+        public static readonly string Main_Stage2 = "Main_Stage2";
+        public static readonly string Main_Stage3 = "Main_Stage3";
+        public static readonly string Main_Stage4 = "Main_Stage4";
+        public static readonly string Main_Stage5 = "Main_Stage5";
+        public static readonly string Main_Stage6 = "Main_Stage6";
+        public static readonly string Result = "Result";
+    }
+}
