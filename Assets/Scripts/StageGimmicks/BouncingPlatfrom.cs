f454c484a21d1cce1610f9d38446a0c0e13edbad
504-38\ktc <ktc23a31a0017@edu.kyoto-tech.ac.jp>
2024-06-28 11:16:48 +0900

    風船のエフェクトの出現位置調整

--- Assets/Scripts/StageGimmicks/BouncingPlatfrom.cs
+++ Assets/Scripts/StageGimmicks/BouncingPlatfrom.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class BouncingPlatfrom : MonoBehaviour
 {
-    public float bounceForce = 10f; // ’µ‚Ë‚é—Í
+    [SerializeField] private float bounceForce = 10f; // ’µ‚Ë‚é—Í
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (!collision.TryGetComponent<Rigidbody2D>(out var rb2d)) { return; }
 
-        EffectContainer.Instance.PlayEffect(GeneralSettings.Instance.Prehab.BalloonEffect, transform.position);
+        var effectPos = collision.transform.position + transform.position;
+        effectPos /= 2;
+
+        EffectContainer.Instance.PlayEffect(GeneralSettings.Instance.Prehab.BalloonEffect, effectPos);
         rb2d.velocity = new Vector2(rb2d.velocity.x, bounceForce);
     }
 }
