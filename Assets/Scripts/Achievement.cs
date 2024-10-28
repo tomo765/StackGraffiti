42152391a5925789ea69da226a5f733fefe7e5c9
TIchi3310 <ktc2241010003@edu.kyoto-tech.ac.jp>
2024-10-28 19:40:19 +0900

    steam実績用セットアップ

---
+++ Assets/Scripts/Achievement.cs
@@ -0,0 +1,17 @@
+using Steamworks;
+using UnityEngine;
+
+public class Achievement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        EndingAchievement();
+    }
+
+    //　エンディング(クレジット)を見たら実績解除
+    public void EndingAchievement()
+    {
+
+    }
+}
