f21516a94e9972394ca20d305aac266967544755
TIchi3310 <ktc2241010003@edu.kyoto-tech.ac.jp>
2024-10-28 20:44:17 +0900

    実績解除を試みたスクリプト(EndingAchievement)追加

--- Assets/Scripts/Achievement.cs
+++ Assets/Scripts/Achievement.cs
@@ -1,9 +1,9 @@
 using Steamworks;
 using UnityEngine;
 
+// タイトルシーンのAchievementManagerオブジェクトでエンディングを見たか判定して、見た場合エンディングを見た実績解除がされるようにしているスクリプトです。
 public class Achievement : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         EndingAchievement();
@@ -12,6 +12,23 @@ public class Achievement : MonoBehaviour
     //　エンディング(クレジット)を見たら実績解除
     public void EndingAchievement()
     {
+        // エンディングを見た判定になったら
+        if (StageDataUtility.StageDatas.SawCredit == true)
+        {
+            // エンディングアチーブメント解除
+            if(SteamManager.Initialized) 
+            {
+                SteamUserStats.RequestCurrentStats();
+                SteamUserStats.SetAchievement("ENDING_ACHIEVEMENT");
+                SteamUserStats.StoreStats();
+
+                SteamUserStats.RequestCurrentStats();
+                bool isCleared; // 達成なら true, 未達成なら false が入ります
+                SteamUserStats.GetAchievement(name.ToString(), out isCleared);
+            }
+        }
 
     }
+
+
 }
