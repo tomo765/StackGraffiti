f1f6ddf7696621698ddfdab42ec1bdcf14a5425c
Nakaya Shunto <ktc2231020001@edu.kyoto-tech.ac.jp>
2024-11-20 19:18:13 +0900

    ステージクリアしたときに遊び方のパネルを閉じるようにした

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -92,6 +92,7 @@ public static class GameManager
         SoundManager.Instance?.PlayNewSE(GeneralSettings.Instance.Sound.ClearSE);
 
         StageDataUtility.SetStageScore(m_CullentStage, m_SleepCount);
+        DontDestroyCanvas.Instance.InvisibleAllUI();
         DontDestroyCanvas.Instance.ChangeResultUIVisible(true);
         DontDestroyCanvas.Instance.ResultUI.SetStarLevel();
     }
