010a8b9f2790db0994d381ce17064596aecf7dbd
Nakaya Shunto <ktc2231020001@edu.kyoto-tech.ac.jp>
2024-11-25 00:47:37 +0900

    メッシュ生成の処理を軽くした

--- Assets/Scripts/Characters/CharacterCreator.cs
+++ Assets/Scripts/Characters/CharacterCreator.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 /// <summary> キャラの生成に関する設定や処理 </summary>
@@ -40,10 +41,11 @@ public static class CharacterCreator  //参考サイト : https://qiita.com/divideby_
     /// <summary> キャラを書ける状態でマウス左をホールドしてる時の処理 </summary>
     public static void OnHold(Vector2 position)
     {
-        var pos = InputExtension.WorldMousePos - position;
-        m_Vlist.Add(pos);
-
-        CreateMesh();
+        AddMesh(InputExtension.WorldMousePos - position);
+        
+        //var pos = InputExtension.WorldMousePos - position;
+        //m_Vlist.Add(pos);
+        //CreateMesh();
     }
 
     /// <summary> キャラを書いているときにマウス左を話した時の処理 </summary>
@@ -121,6 +123,43 @@ public static class CharacterCreator  //参考サイト : https://qiita.com/divideby_
 
         m_Mesh.SetVertices(vertices);
         m_Mesh.SetIndices(indices.ToArray(), MeshTopology.Quads, 0);
+    }
+
+    private static void AddMesh(Vector2 addVec)
+    {
+        SetVertices();
+        SetIndices();
+
+        void SetVertices()
+        {
+            m_Vlist.Add(addVec);
+            if(m_Vlist.Count < 2) { return; }
+
+            var vertices = m_Mesh.vertices.ToList();
+
+            var delta = m_Vlist[m_Vlist.Count - 2] - m_Vlist[m_Vlist.Count - 1];
+            if (delta.magnitude < 0.01f) { return; }
+
+            var normalVec = new Vector2(-delta.y, delta.x).normalized * m_LineWidth;
+
+            vertices.Add(m_Vlist[m_Vlist.Count - 2] - normalVec);
+            vertices.Add(m_Vlist[m_Vlist.Count - 2] + normalVec);
+
+            m_Mesh.SetVertices(vertices);
+        }
+        void SetIndices()
+        {
+            var vertCount = m_Mesh.vertices.Length;
+            if (vertCount < 4) { return; }
+            var indices = m_Mesh.GetIndices(0).ToList();
+
+            indices.Add(vertCount - 4);
+            indices.Add(vertCount - 2);
+            indices.Add(vertCount - 1);
+            indices.Add(vertCount - 3);
+
+            m_Mesh.SetIndices(indices.ToArray(), MeshTopology.Quads, 0);
+        }
     }
 
     /// <summary> 現在の見た目から当たり判定を作る </summary>
